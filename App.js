acd1ba7d4db2f1226882ab180136b71f60e90be2
QuanDao <daonguyenquan@hotmail.com>
2019-01-11 11:21:02 +0700

    goBack

--- App.js
+++ App.js
@@ -1,15 +1,47 @@
+import {BackHandler, WebView} from 'react-native'
 import React, {Component} from 'react';
-import {WebView} from 'react-native';
 
 class MyWeb extends Component {
-  render() {
-      return (
-          <WebView
-              source={{uri: 'http://10.0.2.2:3000/index'}}
-              style={{marginTop: 23}}
-        />
-      );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            canGoBack: false
+        }
+    }
+
+    render() {
+        return (
+            <WebView
+                source={{uri: 'http://10.0.2.2:3000/index'}}
+                style={{marginTop: 23}}
+                ref={(myWeb) => this.refWeb = myWeb}
+                onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+            />
+        );
+    }
+
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
+    }
+
+    handleBackPress = () => {
+        if (this.state.canGoBack) {
+            this.refWeb.goBack();
+        } else {
+            this.props.navigation.goBack(null)
+        }
+        return true;
+    };
+
+    onNavigationStateChange(navState) {
+        this.setState({
+            canGoBack: navState.canGoBack
+        });
+    }
 }
 
 export default MyWeb;
